60944a2b73513d73cfd74e2445bc9ba65cf1a852
naufalaufa <muhammadnaufalaufarifqi@gmail.com>
2026-01-11 21:31:45 +0700

    add git ignore folder in backend section

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,10 @@ app.use(cors({
 app.use('/public/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',            
-    password: 'root',
-    database: 'tabungan_zipal'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
